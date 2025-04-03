8d31b183bb57d2c3d259ef68a56f38f444c9f97f
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2025-04-03 08:48:29 -0400

    add `database.js` and `.env` files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,4 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://172.17.255.255:3000/api/v1/status");
-  console.log(response);
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
