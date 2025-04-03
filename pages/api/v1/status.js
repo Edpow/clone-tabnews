b40ac1588399a3d8362bd512abd701f1c41a9966
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2025-04-03 10:37:19 -0400

    add services scripts

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 3+3 as sum;");
   console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  response.status(200).json({ valor: result.rows });
 }
 
 export default status;
