39c9dc5e3a1f62b7290401a7f0637ec54e4ac4c5
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2025-06-16 16:15:15 -0400

    feat: define `Set-Cookie` header on new session

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -1,6 +1,7 @@
 import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
 import session from "models/session";
+import setCookieParser from "set-cookie-parser";
 
 beforeAll(async () => {
   await orchestrator.waitFormAllServices();
@@ -135,6 +136,16 @@ describe("POST /api/v1/sessions", () => {
       expect(expires_at - created_at).toEqual(
         session.EXPIRATION_IN_MILLISECONDS,
       );
+
+      const parsedSetCookie = setCookieParser(response, { map: true });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: responseBody.token,
+        path: "/",
+        httpOnly: true,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+      });
     });
   });
 });
