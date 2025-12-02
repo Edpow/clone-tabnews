f771e8ee86a108f62c209397f381f559c99d2abb
Edgar Rocha <edgar.rocha.dev@outlook.com>
2025-12-02 19:35:28 -0400

    feat: add `email` model

--- infra/email.js
+++ infra/email.js
@@ -2,11 +2,18 @@ import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_SMTP_HOST,
+    port: process.env.EMAIL_SMTP_PORT,
+    auth: {
+        user: process.env.EMAIL_SMTP_USER,
+        pass: process.env.EMAIL_SMTP_PASSWORD,
+    },
+    secure: process.env.NODE_ENV !== 'production' ? false : true
 })
 
-async function send() {
-
+async function send(mailOptions) {
+    await transporter.sendMail(mailOptions)
 }
+
 
 const email = {
     send
