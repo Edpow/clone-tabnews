b25d627b960c6d59bd1ba7cae01436e75ec3890b
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2026-01-22 10:41:35 -0400

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/_use-cases/registration-flow.test.js
+++ tests/_use-cases/registration-flow.test.js
@@ -82,7 +82,20 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login", async () => {});
+  test("Login", async () => {
+    const loginResponse = await fetch(`http://localhost:3000/api/v1/sessions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "registration.flow@email.com",
+        password: "123456",
+      }),
+    });
+
+    expect(loginResponse.status).toBe(201);
+  });
 
   test("Get user information", async () => {});
 });
