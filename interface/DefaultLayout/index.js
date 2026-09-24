4f73206c763e63a3eb7506771f394daadd405e8f
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2026-09-24 11:00:36 -0400

    feat: use primer in DefaultLayout

--- interface/DefaultLayout/index.js
+++ interface/DefaultLayout/index.js
@@ -1,11 +1,26 @@
-import { PageLayout } from "@primer/react";
+import { Header, PageLayout, Text } from "@primer/react";
 
 export default function DefaultLayout({ children }) {
   return (
-    <PageLayout>
-      <PageLayout.Header>Cabeçalho</PageLayout.Header>
-      <PageLayout.Content>{children}</PageLayout.Content>
-      <PageLayout.Footer>Conteúdo</PageLayout.Footer>
-    </PageLayout>
+    <>
+      <Header>
+        <Header.Item full>
+          <Header.Link href="/">TabNews</Header.Link>
+        </Header.Item>
+        <Header.Item>
+          <Header.Link href="/login">Login</Header.Link>
+        </Header.Item>
+        <Header.Item>
+          <Header.Link href="/cadastro">Cadastrar</Header.Link>
+        </Header.Item>
+      </Header>
+
+      <PageLayout>
+        <PageLayout.Content>{children}</PageLayout.Content>
+        <PageLayout.Footer divider="line">
+          <Text size="small">© {new Date().getFullYear()} TabNews</Text>
+        </PageLayout.Footer>
+      </PageLayout>
+    </>
   );
 }
