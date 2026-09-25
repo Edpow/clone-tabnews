f5cbc81747205f97da94122d283a3c6fe3841916
Edgar R. Arruda <edgar.rocha.dev@outlook.com>
2026-09-25 08:52:30 -0400

    refactor: add Banner in /cadastro/confirmar page

--- pages/cadastro/confirmar/index.js
+++ pages/cadastro/confirmar/index.js
@@ -1,9 +1,17 @@
+import { Banner } from "@primer/react";
 import DefaultLayout from "interface/DefaultLayout";
 
 export default function ConfimRegisterPage() {
   return (
-    <DefaultLayout>
-      <h1>Confira seu email</h1>
+    <DefaultLayout
+      metadata={{ title: "Confirme seu email" }}
+      contentWidth={"small"}
+    >
+      <Banner
+        variant="warning"
+        title="Confira seu email"
+        description="Abra o emeil enviado pelo TabNews e clique no link de confirmação"
+      />
     </DefaultLayout>
   );
 }
